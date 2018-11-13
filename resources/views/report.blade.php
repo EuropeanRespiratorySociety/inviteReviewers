e125370889f29baff4efa4148b828ce0e2b8c3ff
idealley <samuelpouyt@gmail.com>
2018-11-13 10:46:06 +0100

    added https support

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -2,27 +2,27 @@
 <html>
 <head>
     {!! HTML::style('css/all.css') !!}
-<link rel="stylesheet" href="http://tools.ersnet.org/ersKit/0.4/fonts/fonts.min.css" />
+<link rel="stylesheet" href="https://tools.ersnet.org/ersKit/0.4/fonts/fonts.min.css" />
 <meta id="token" name="token" value="{{ csrf_token() }}"></meta>
     <title>Reports</title>
 
 
 
         <!--[if (gte IE 6)&(lte IE 8)]>
-        <link rel="stylesheet" href="http://tools.ersnet.org/ersKit/0.4/css/ie8.css" />
-        <script src="http://tools.ersnet.org/ersKit/0.4/js/respond.js"></script> 
-        <link href="http://tools.ersnet.org/ersKit/0.4/cross-domain/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
+        <link rel="stylesheet" href="https://tools.ersnet.org/ersKit/0.4/css/ie8.css" />
+        <script src="https://tools.ersnet.org/ersKit/0.4/js/respond.js"></script> 
+        <link href="https://tools.ersnet.org/ersKit/0.4/cross-domain/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
         <link href="/cross-domain/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />
         <script src="/cross-domain/respond.proxy.js"></script>
-        <script type="text/javascript" src="http://tools.ersnet.org/ersKit/0.4/js/selectivizr.js"></script>
-        <script src="http://tools.ersnet.org/ersKit/0.4/js/ie8.js"></script>
+        <script type="text/javascript" src="https://tools.ersnet.org/ersKit/0.4/js/selectivizr.js"></script>
+        <script src="https://tools.ersnet.org/ersKit/0.4/js/ie8.js"></script>
         <![endif]-->
 
 </head>
 <body>
 <div class="ers-main-container">
 <div class="ers-metanavigation uk-visible-large">
-    <a class="ers-logo uk-visible-large" href="http://www.ersnet.org"></a>
+    <a class="ers-logo uk-visible-large" href="https://www.ersnet.org"></a>
 </div>
 <div class="ers-container" style="left: 100px;">
     @include('navbar')
